e603599c159f95e1ac26e573bc112172094c8be6
Rajnish <rajnish993123@gmail.com>
2025-01-15 03:16:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,3 +25,12 @@ function toggleRule(element) {
 document.getElementById('logo').onerror = function() {
   this.src = 'https://cdn.discordapp.com/attachments/1288526364286255124/1328633512424636447/IMG-20250114-WA0009.jpg?ex=678769e2&is=67861862&hm=4122330637044c220f01c90551d21e5ac078973c8f58ccca3ca80b52c3925366&';
 };
+document.addEventListener('copy', (event) => {
+            // Create a custom message
+            const customMessage = `https://knarlix.github.io/Universe/`;
+            
+            // Set the new clipboard data
+            event.clipboardData.setData('text/plain', customMessage);
+            // Prevent the default copy action
+            event.preventDefault();
+        });
