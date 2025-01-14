9016f83eacc865566ae0a3d0fd0e9aba792133da
Rajnish <rajnish993123@gmail.com>
2025-01-14 22:41:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,4 @@ function toggleRule(element) {
   }
 }
 
-// Logo fallback
-document.getElementById('logo').onerror = function() {
-  this.src = 'https://cdn.discordapp.com/attachments/1288526364286255124/1328633512424636447/IMG-20250114-WA0009.jpg?ex=678769e2&is=67861862&hm=4122330637044c220f01c90551d21e5ac078973c8f58ccca3ca80b52c3925366&';
-};
+
